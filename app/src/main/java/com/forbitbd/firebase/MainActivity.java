0f8bf21b14b6a7cef39968e1c19cf537288c3509
Saimulhoque <saimulhoque8217@gmail.com>
2021-03-27 16:55:50 +0600

    Initial commit

---
+++ app/src/main/java/com/forbitbd/firebase/MainActivity.java
@@ -0,0 +1,97 @@
+package com.forbitbd.firebase;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<User> userList;
+    UserAdapter adapter;
+    RecyclerView recyclerView;
+    DatabaseReference databaseReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerview);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        userList = new ArrayList<>();
+        databaseReference = FirebaseDatabase.getInstance().getReference("user");
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    User user = snapshot.getValue(User.class);
+                    userList.add(user);
+                }
+
+                adapter=new UserAdapter(getApplicationContext(),userList);
+                recyclerView.setAdapter(adapter);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
+
+//        FirebaseRecyclerOptions<User> options =
+//                new FirebaseRecyclerOptions.Builder<User>()
+//                        .setQuery(FirebaseDatabase.getInstance().getReference().child("users"), User.class)
+//                        .build();
+
+//        adapter = new UserAdapter(options);
+//        UserAdapter adapter = new UserAdapter(this,userList);
+//        recyclerView.setAdapter(adapter);
+//        DatabaseReference myDatabaseRef = FirebaseDatabase.getInstance().getReference().child("users");
+//        myDatabaseRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                userList.clear();
+//                for (DataSnapshot snapshot : dataSnapshot .getChildren()){
+//                    userList.get(Integer.parseInt(snapshot.getValue().toString()));
+//                }
+//                adapter.notifyDataSetChanged();
+//            }
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
+    }
+
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        adapter.startListening();
+//    }
+//
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        adapter.stopListening();
+//    }
+}
---
+++ app/src/main/java/com/forbitbd/firebase/UserAdapter.java
@@ -0,0 +1,58 @@
+package com.forbitbd.firebase;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserHolder> {
+
+    Context context;
+    List<User> userList;
+
+    public UserAdapter(Context context, List<User> userList) {
+        this.context = context;
+        this.userList = userList;
+    }
+
+    @NonNull
+    @Override
+    public UserHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+       View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_user,parent,false);
+       return new UserHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserHolder holder, int position) {
+       User user = userList.get(position);
+        holder.text1.setText(user.getName());
+        holder.text2.setText(user.getEmail());
+        holder.text3.setText(user.getPhone());
+    }
+
+    @Override
+    public int getItemCount() {
+        return userList.size();
+    }
+
+    public class UserHolder extends RecyclerView.ViewHolder {
+        TextView text1, text2, text3;
+
+        public UserHolder(@NonNull View itemView) {
+            super(itemView);
+            text1 = itemView.findViewById(R.id.name);
+            text2 = itemView.findViewById(R.id.email);
+            text3 = itemView.findViewById(R.id.phone);
+        }
+    }
+}
